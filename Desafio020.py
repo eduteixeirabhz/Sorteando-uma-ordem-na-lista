bf690ad6d5e664483aabb856c55450e1f580abbc
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 13:34:39 -0300

    Add files via upload

---
+++ Desafio020.py
@@ -0,0 +1,11 @@
+# import random
+from random import shuffle
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+lista = [n1, n2, n3, n4]
+#random.shuffle(lista)
+shuffle(lista)
+print('A ordem de apresentação será: ')
+print(lista)
